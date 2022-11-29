531dc0691a511d02d7c222e5f7670552e3440977
Dmitry Vasilkov <mityavasilkov@gmail.com>
2022-11-29 22:06:25 +0100

    .

---
+++ HW_28.11/src/Main.java
@@ -0,0 +1,25 @@
+//Вывести все 8 примитивных типов на консоль
+public class Main {
+    public static void main(String[] args) {
+        int a = 300000000;
+
+        long b = 7777777777777777777L;
+
+        short c = 8778;
+
+        byte d = 123;
+
+        double e = 12.5;
+
+        float f = 17.1f;
+
+        char g = 'c';
+
+        boolean h = true;
+
+        System.out.printf("%d, %d, %d, %d, %f, %f, %c, %b", a, b, c, d, e, f, g, h);
+
+
+    }
+
+}
